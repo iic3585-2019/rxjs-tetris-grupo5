77553dc547e8aafadb8e1b708fb4d64ef8e75c13
Francisco <fco.olivaress@gmail.com>
2019-03-26 17:47:02 -0300

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
